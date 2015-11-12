94316b17433ed35e617773e141c03a45763457e7
bifangyu <baofengyv@gmail.com>
2015-11-12 15:49:51 +0800

    增加IE8下的样式和重构代码

--- radio－button－单选按钮/test.js
+++ radio－button－单选按钮/test.js
@@ -1,8 +1,15 @@
 $(function() {
+	/***************************************************************************
+	 *** 单选按钮的初始化 和注册单击事件
+	 **************************************************************************/
+	// 初始化默认单选按钮为选中
+	$(".radio-btn-group input:checked").each(function(index) {
+		//找到for属性为此input元素的ID的label 为其增加active 类
+		$(".radio-btn-group label[for='" + this.id + "']").addClass("active");
+	});
+	// 单击单选按钮事件切换显示样式
 	$(".radio-btn-group .radio-btn").click(function() {
-//		debugger;
-		var parent = $(this).parent(".radio-btn-group");
-		$('label',parent).removeClass("active");
-		$(this).addClass("active");				
+		$(this).siblings("label").removeClass("active");
+		$(this).addClass("active");
 	});
 });
